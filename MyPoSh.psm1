444f52743d90fb218d35fefd8c5861d2af066ee0
lijin <lijin.thu@outlook.com>
2017-06-17 23:12:00 +0900

    add ls extend

--- MyPoSh.psm1
+++ MyPoSh.psm1
@@ -170,3 +170,105 @@ param (
     }
     $repos
 }
+
+New-CommandWrapper Out-Default -Process {
+  $regex_opts = ([System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
+  $compressed = New-Object System.Text.RegularExpressions.Regex(
+    '\.(zip|tar|gz|rar|jar|war)$', $regex_opts)
+  $executable = New-Object System.Text.RegularExpressions.Regex(
+    '\.(exe|bat|cmd|msi|ps1|psm1|vbs|reg)$', $regex_opts)
+  $text_files = New-Object System.Text.RegularExpressions.Regex(
+    '\.(txt|doc|docx|vim|cfg|conf|ini|csv|log)$', $regex_opts)
+  $image_files = New-Object System.Text.RegularExpressions.Regex(
+    '\.(bmp|jpg|png|gif|jpeg|ps)$', $regex_opts)
+  $media_files = New-Object System.Text.RegularExpressions.Regex( 
+    '\.(mp4|mpg|mkv|avi|mp3|wave|ape|flac)$', $regex_opts)
+  $source_files = New-Object System.Text.RegularExpressions.Regex( 
+    '\.(py|pl|cs|rb|h|cpp|c)$', $regex_opts)
+  $office_files = New-Object System.Text.RegularExpressions.Regex( 
+    '\.(ppt|pptx|pdf|xl|xlsx|xls)$', $regex_opts)
+
+  if(($_ -is [System.IO.DirectoryInfo]) -or ($_ -is [System.IO.FileInfo]))
+  {
+    if(-not ($notfirst))
+    {
+      Write-Host "`n    Directory: " -noNewLine
+      Write-Host "$(pwd)`n" -foregroundcolor "Cyan"
+      Write-Host "Mode        Last Write Time       Length   Name"
+      Write-Host "----        ---------------       ------   ----"
+      $notfirst=$true
+    }
+
+    if ($_ -is [System.IO.DirectoryInfo])
+    {
+      Write-Host ("{0}   {1}                {2}" -f $_.mode, ([String]::Format("{0,10} {1,8}", $_.LastWriteTime.ToString("d"), $_.LastWriteTime.ToString("t"))), $_.name) -ForegroundColor "Cyan"
+    }
+    else
+    {
+      if ($compressed.IsMatch($_.Name)) {
+        $color = "DarkGreen"
+      } elseif ($executable.IsMatch($_.Name)) {
+        $color =  "Red"
+      } elseif ($text_files.IsMatch($_.Name)) {
+        $color =  "Yellow"
+      } elseif ($image_files.IsMatch($_.Name)) {
+        $color =  "DarkCyan"
+      } elseif ($media_files.IsMatch($_.Name)) {
+        $color =  "DarkGray"
+      } elseif ($source_files.IsMatch($_.Name)) {
+        $color =  "Magenta"
+      } elseif ($office_files.IsMatch($_.Name)) {
+        $color =  "Green"
+      } else {
+        $color = "White"
+      }
+      Write-Host ("{0}   {1}   {2,10}   {3}" -f $_.mode, ([String]::Format("{0,10} {1,8}", $_.LastWriteTime.ToString("d"), $_.LastWriteTime.ToString("t"))), $_.length, $_.name) -ForegroundColor $color
+    }
+    $_ = $null
+  }
+} -end {
+ # Write-Host
+}
+
+function Get-DirSize
+{
+  param ($dir)
+  $bytes = 0
+  $count = 0
+
+  Get-Childitem $dir | Foreach-Object {
+    if ($_ -is [System.IO.FileInfo])
+    {
+      $bytes += $_.Length
+      $count++
+    }
+  }
+
+  Write-Host "`n    " -NoNewline
+
+  if ($bytes -ge 1KB -and $bytes -lt 1MB) {
+    Write-Host ("" + [Math]::Round(($bytes / 1KB), 2) + " KB") -ForegroundColor "White" -NoNewLine
+  } elseif ($bytes -ge 1MB -and $bytes -lt 1GB) {
+    Write-Host ("" + [Math]::Round(($bytes / 1MB), 2) + " MB") -ForegroundColor "White" -NoNewLine
+  } elseif ($bytes -ge 1GB) {
+    Write-Host ("" + [Math]::Round(($bytes / 1GB), 2) + " GB") -ForegroundColor "White" -NoNewLine
+  } else {
+    Write-Host ("" + $bytes + " bytes") -ForegroundColor "White" -NoNewLine
+  }
+  Write-Host " in " -NoNewline
+  Write-Host $count -ForegroundColor "White" -NoNewline
+  Write-Host " files"
+}
+
+function Get-DirWithSize
+{
+  param ($dir)
+  Get-Childitem $dir
+  Get-DirSize $dir
+}
+
+Remove-Item alias:dir
+Remove-Item alias:ls
+Set-Alias dir Get-DirWithSize
+Set-Alias ls Get-DirWithSize
+
